819e382b191ae196469b8f6908df30c108fa6b44
Sushil Kumar <baymax732001@gmail.com>
2022-06-30 22:23:17 +0530

    Calling a API from a external Website Using Http

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+
+Location myLocation = Location();
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,56 +13,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     // TODO: implement initState
-    _determinePosition();
-    getLocation();
+
     super.initState();
+    myLocation.getPermission();
+    myLocation.getCurrentLocation();
+
+    print('App has been deactivated');
   }
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
-  }
-
-  void getLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          forceAndroidLocationManager: true,
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } on Exception catch (e) {
-      print(e);
+   void getData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f267658008c4ceed2e43cecfb2e5087f'),
+    );
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
     }
   }
 
@@ -67,10 +37,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void deactivate() {
     // TODO: implement deactivate
     super.deactivate();
+    print('App has been deactivated');
   }
+
+ 
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(),
     );
